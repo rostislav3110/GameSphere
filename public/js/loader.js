0d2b03fc33827dd605588c66e4e392982aa2d708
rostik3110 <rostikzero3110.2004@gmail.com>
2023-11-22 01:47:10 +0200

    udaptation

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,6 +1,5 @@
 setTimeout(() =>{
     let preloader = document.getElementById('page-preloader');
-
     if(preloader && !preloader.classList.contains('done') ) {
         preloader.classList.add('done')
     }
