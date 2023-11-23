9f069a99d7e5b002bcd242357e17d33cc2bd13dd
rostik3110 <rostikzero3110.2004@gmail.com>
2023-11-23 02:18:38 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/main.scss';
 import games from './assets/games.json';
 import video_guide from './assets/video-guide.json';
 import postPublic from './assets/posts.json';
+import storeGame from './assets/store_game.json';
 
 import mainTemplate from './pages/main.hbs';
 import guideTemplate from './pages/guide.hbs';
@@ -16,7 +17,7 @@ const templates = {
   '/store': storeTemplate
 };
 
-const pageData = { games, video_guide, postPublic };
+const pageData = { games, video_guide, postPublic, storeGame };
 const targetElement = document.getElementById('app');
 
 const render = (template, data, targetElement) => {
